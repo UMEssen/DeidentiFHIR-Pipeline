f91005609cb151f74afe5f99a63497595a068f88
Roland Ihle <roland.ihle@uk-essen.de>
2025-05-06 17:07:33 +0200

    file cohort selection modified, defining delimiter is possible now

--- src/main/java/de/ume/deidentifhirpipeline/config/cohortselection/FileCohortSelectionConfig.java
+++ src/main/java/de/ume/deidentifhirpipeline/config/cohortselection/FileCohortSelectionConfig.java
@@ -7,4 +7,5 @@ import lombok.Setter;
 @Setter
 public class FileCohortSelectionConfig {
   String path;
+  String delimiter;
 }
--- src/main/java/de/ume/deidentifhirpipeline/transfer/cohortselection/FileCohortSelection.java
+++ src/main/java/de/ume/deidentifhirpipeline/transfer/cohortselection/FileCohortSelection.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 @Component("cohort-selection.via-file")
@@ -13,7 +14,12 @@ public class FileCohortSelection implements CohortSelection {
   @Override
   public List<String> before(ProjectConfig projectConfig) throws Exception {
     String path = projectConfig.getCohortSelection().getViaFile().getPath();
+    String delimiter = projectConfig.getCohortSelection().getViaFile().getDelimiter();
     Path filePath = Paths.get(path);
-    return Files.readAllLines(filePath).stream().filter(s -> !s.isBlank()).toList();
+    String content = Files.readString(filePath);
+    return Arrays.stream(content.split(delimiter))
+        .filter(s -> !s.isBlank())
+        .map(s -> s.trim())
+        .toList();
   }
 }
